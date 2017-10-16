020aea39a4927c7fd20ca05386ae2eab43e643be
Javier Ayala <jayala@rackspace.com>
2017-10-16 10:19:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# `rswebteam.security-server-config`
+
+This role applies generic security-related server configuration changes.  These
+may be resulting from TVA security scans of our environment, or best
+practices.
+
+## Requirements
+
+None
+
+## Role Variables
+
+From the `defaults/main.yml` file:
+
+```yaml
+# Disable ICMP Redirects
+rax_svr_cfg_icmp_redirect: 0
+```
+
+## Dependencies
+
+None
+
+## Example Playbook
+
+```yaml
+    - hosts: servers
+      roles:
+         - { role: rswebteam.security-server-config, rax_svr_cfg_icmp_redirect: 1 }
+```
+
+## Author Information
+
+Javier Ayala
---
+++ molecule/default/tests/test_sysctl.py
@@ -0,0 +1,24 @@
+"""Test the changes in sysctl.conf."""
+import os
+import re
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('config_line', [
+    # Disable ICMP Redirects
+    'net.ipv4.conf.all.accept_redirects(\s+)?=(\s+)?0'
+])
+def test_sysctl_file(host, config_line):
+    """Test that certain entries exist in the file."""
+    f = host.file('/etc/sysctl.conf')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    regex = re.escape(config_line)
+    assert f.contains('^{}$'.format(regex))
